8abdf8067afc578ff700916b501bd1d0c504e473
QL5251 <5251QL@OUTLOOK.COM.com>
2026-06-04 16:52:05 +0800

    清理根目录重复文件（已归档至02_pointers）

--- sizeof_test.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("int zhanyong %d zijie\n",(int)sizeof(int));
-    printf("char zhanyong %d zijie\n",(int)sizeof(char));
-    printf("double zhanyong %d zijie\n",(int)sizeof(double));
-    return 0;
-}
