ae3266eb1a05fb4648a0a473abff5f8c4f8d64e1
QL5251 <5251QL@OUTLOOK.COM.com>
2026-06-04 16:29:15 +0800

    2026-06-04: 指针基础+swap值传递vs地址传递+sizeof各类型

---
+++ sizeof_test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("int zhanyong %d zijie\n",(int)sizeof(int));
+    printf("char zhanyong %d zijie\n",(int)sizeof(char));
+    printf("double zhanyong %d zijie\n",(int)sizeof(double));
+    return 0;
+}
